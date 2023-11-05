ace2b6d02da040f7f834995672517b17495b5539
ashish-metapolis <vattevaii@gmail.com>
2023-11-05 19:21:32 +0545

    fix: deferring gtm.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -41,7 +41,7 @@ export default function App({ Component, pageProps }: AppProps) {
           w[l] = w[l] || [];
           w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
           var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-          j.async=true;
+          j.defer=true;
           j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
           f.parentNode.insertBefore(j,f);
         })
