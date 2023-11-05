7193fda0052aa764bba27600f35030ec0dc175b1
ashish-metapolis <vattevaii@gmail.com>
2023-11-05 13:32:23 +0545

    fix: gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,27 +5,33 @@ import Script from 'next/script'
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     <Script
-      strategy="lazyOnload"
+
       src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
     />
 
-    <Script id="googleTagManager" strategy="lazyOnload">
+    <Script id="googleTagManager">
       {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+      `}
     </Script>
     {/* <!-- Google Tag Manager --> */}
-    <Script id="gtagmanager" strategy='lazyOnload'>
-      {`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=      
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})
-      (window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_M}');`}
+    <Script id="gtagmanager">
+      {`
+        (function(w,d,s,l,i){
+          w[l] = w[l] || [];
+          w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+          var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+          j.async=true;
+          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+          f.parentNode.insertBefore(j,f);
+        })
+        (window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_M}');
+      `}
     </Script>
     {/* <!-- End Google Tag Manager --> */}
     <Component {...pageProps} />
