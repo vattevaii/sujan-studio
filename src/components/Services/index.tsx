a96010a8c1660d82c34fe96e17218e8b6a8dffef
ashish-metapolis <vattevaii@gmail.com>
2023-11-05 13:46:13 +0545

    fix: structure of page

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -37,6 +37,7 @@ function Service({ service }: { service: Service }) {
 
 export default function Services({ services }: Props) {
     return (<>
+        <h2 className="absolute invisible h-0">Our Services</h2>
         {services.map((service, idx) => <Service key={idx} service={service} />)}
     </>)
 }
