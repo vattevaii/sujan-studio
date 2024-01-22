8e6b8f3b285940cec842917612846a8813ddb75e
Ashish <45767581+vattevaii@users.noreply.github.com>
2024-01-22 08:05:36 +0545

    fix : action btn tel not working

--- src/modules/CallToAction/ActionButtons.tsx
+++ src/modules/CallToAction/ActionButtons.tsx
@@ -10,26 +10,29 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 
-type Props = {mobileNumber: string};
+type Props = { mobileNumber: string };
 const icons = svgs.callToActionSvgs;
-export default function ActionButtons({mobileNumber}: Props) {
+export default function ActionButtons({ mobileNumber }: Props) {
   const router = useRouter();
   return (
     <div className="flex flex-col-reverse gap-3 fixed z-10 bottom-0 right-0 m-5">
       <TooltipWrapper>
-        <TooltipTrigger>
-          <Link
-                href={`tel:${mobileNumber?.replace(/\D/g, "")}`} className="rounded-full p-4 bg-light-grey shadow-around block">
-            <Image
-              width="35"
-              height="35"
-              className="w-5 h-5"
-              src={icons.bookUs.src}
-              alt=""
-            ></Image>
-          </Link>
-          <TooltipContent>Call Us Now</TooltipContent>
-        </TooltipTrigger>
+        <Link
+          href={`tel:${mobileNumber?.replace(/\D/g, "")}`}
+          
+        >
+          <TooltipTrigger>
+            <div className="rounded-full p-4 bg-light-grey shadow-around block">
+              <Image
+                width="35"
+                height="35"
+                className="w-5 h-5"
+                src={icons.bookUs.src}
+                alt=""
+              ></Image>
+              <TooltipContent>Call Us Now</TooltipContent>
+            </div></TooltipTrigger>
+        </Link>
       </TooltipWrapper>
       <TooltipWrapper>
         <TooltipTrigger onClick={() => router.push("/book-us")}>
